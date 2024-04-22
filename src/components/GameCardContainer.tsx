67a9a0c19c45938b61ca746303632da9dc6e8a2b
runkai <runkaijiang@foxmail.com>
2024-04-22 19:49:47 -0400

    nav done, proper spacing

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -2,7 +2,7 @@ import { Card } from "@chakra-ui/react";
 import { ReactNode } from "react";
 function GameCardContainer({ children }: { children: ReactNode }) {
     return (
-        <Card width="300px" height="320px" borderRadius={10} overflow="hidden">
+        <Card height="320px" borderRadius={10} overflow="hidden">
             {children}
         </Card>
     );
