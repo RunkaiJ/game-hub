199efa6c121cd56c2126977a7052d6eeaf914a43
runkai <runkaijiang@foxmail.com>
2024-04-22 20:06:01 -0400

    show spinner while fetching genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -2,7 +2,7 @@ import { Card } from "@chakra-ui/react";
 import { ReactNode } from "react";
 function GameCardContainer({ children }: { children: ReactNode }) {
     return (
-        <Card height="320px" borderRadius={10} overflow="hidden">
+        <Card height="300px" borderRadius={10} overflow="hidden">
             {children}
         </Card>
     );
