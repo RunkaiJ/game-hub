e702c35035a2a2509e06c3d74859ce1c8653ce37
runkai <runkaijiang@foxmail.com>
2024-04-23 01:51:29 -0400

    change game container height

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -2,7 +2,7 @@ import { Card } from "@chakra-ui/react";
 import { ReactNode } from "react";
 function GameCardContainer({ children }: { children: ReactNode }) {
     return (
-        <Card height="330px" borderRadius={10} overflow="hidden">
+        <Card height="350px" borderRadius={10} overflow="hidden">
             {children}
         </Card>
     );
